3e3b4e7ac3c7aa7d1c5cfe13394d1f902942a6ef
Evan <rideburtonsnow3@gmail.com>
2018-08-31 22:58:51 -0600

    added selectors and functioin.js

---
+++ pageObject/homePageUI.js
@@ -0,0 +1,15 @@
+module.exports = {
+    url: 'https://www.decisely.com/',
+    elements: {
+        logo: {
+            selector: '(//img[@alt="Decisely Logo"])[1]',
+            locateStrategy: 'xpath'
+        },
+
+        smallBusinesses: {
+            selector: '(//a[@class="nav-link"])[1]',
+            locateStrategy: 'xpath'
+        },
+        
+    }
+}
