776972839fdf3e4ba39f272680fce590357da9b8
Evan <rideburtonsnow3@gmail.com>
2018-09-03 18:27:22 -0600

    orginized selectors.js

--- sources/selectors.js
+++ sources/selectors.js
@@ -1,13 +1,16 @@
 module.exports = {
+    //xPath
     brokers: '(//a[@class="nav-link"])[2]',
     contactUs: '(//a[@class="nav-link"])[6]',
-    getStarted: 'a[id="openbtn"]',
     enterprise: '(//a[@class="nav-link"])[3]',
-    logIn: 'li[class="nav-item telephone"]',
     logo: '(//img[@class="header-logo"])[1]',
     learnMore: '(//a[@class="btn green-background"])[1]',
     optimized: '(//h1[.="Your Business. Optimized."])[1]',
     resources: '(//a[@class="nav-link"])[5]',
     services: '(//a[@class="nav-link"])[4]',
     smallBusinesses: '(//a[@class="nav-link"])[1]',
+
+    //Css
+    getStarted: 'a[id="openbtn"]',
+    logIn: 'li[class="nav-item telephone"]',
 }
