a7772f7e562315ea0bab680f89c54fe5382383b0
Evan <rideburtonsnow3@gmail.com>
2018-09-03 18:25:28 -0600

    adding code to uiTest function

--- sources/functions.js
+++ sources/functions.js
@@ -6,6 +6,21 @@ const uiTest = (browser) => {
         .useXpath()
         .waitForElementVisible(selectors.logo, 5000)
         .waitForElementVisible(selectors.smallBusinesses, 5000)
+        .waitForElementVisible(selectors.brokers, 5000)
+        .waitForElementVisible(selectors.enterprise, 5000)
+        .waitForElementVisible(selectors.services, 5000)
+        .waitForElementVisible(selectors.resources, 5000)
+        .waitForElementVisible(selectors.contactUs, 5000)
+        .useCss()
+        .waitForElementVisible(selectors.logIn, 5000)
+        .waitForElementVisible(selectors.getStarted, 5000)
+        .useXpath()
+        .waitForElementVisible(selectors.optimized, 5000)
+        .expect.element(selectors.optimized).text.to.equal('Your Business. Optimized.')
+    browser
+        .useXpath()
+        .waitForElementVisible(selectors.learnMore, 5000)
+        
 }
 
 module.exports = {
--- sources/selectors.js
+++ sources/selectors.js
@@ -1,4 +1,13 @@
 module.exports = {
+    brokers: '(//a[@class="nav-link"])[2]',
+    contactUs: '(//a[@class="nav-link"])[6]',
+    getStarted: 'a[id="openbtn"]',
+    enterprise: '(//a[@class="nav-link"])[3]',
+    logIn: 'li[class="nav-item telephone"]',
     logo: '(//img[@class="header-logo"])[1]',
+    learnMore: '(//a[@class="btn green-background"])[1]',
+    optimized: '(//h1[.="Your Business. Optimized."])[1]',
+    resources: '(//a[@class="nav-link"])[5]',
+    services: '(//a[@class="nav-link"])[4]',
     smallBusinesses: '(//a[@class="nav-link"])[1]',
 }
